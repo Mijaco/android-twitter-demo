7e1c6f968166e544cc03a940f04f64961f6ac2e6
Mijaco <casio.soe@gmail.com>
2019-09-21 21:06:15 -0500

    lesson-2

---
+++ app/src/main/java/com/davidchura/tiendavirtual/AcercadeActivity.kt
@@ -0,0 +1,42 @@
+package com.davidchura.tiendavirtual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_acercade.*
+
+class AcercadeActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acercade)
+        rbMision.setOnClickListener(this)
+        rbVision.setOnClickListener(this)
+        rbHistoria.setOnClickListener(this)
+        mostrarMision()
+    }
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.rbMision -> mostrarMision()
+            R.id.rbVision -> mostrarVision()
+            R.id.rbHistoria -> mostrarHistoria()
+        }
+    }
+
+    private fun mostrarHistoria() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.llContenedor,HistoriaFragment())
+            .commit()
+    }
+
+    private fun mostrarVision() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.llContenedor,VisionFragment())
+            .commit()
+    }
+
+    private fun mostrarMision() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.llContenedor,MisionFragment())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/davidchura/tiendavirtual/IniciarSesionActivity.kt
@@ -0,0 +1,65 @@
+package com.davidchura.tiendavirtual
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_iniciar_sesion.*
+
+class IniciarSesionActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnIniciarSesion -> iniciarSesion();
+        }
+    }
+
+    private fun iniciarSesion() {
+        startActivity(Intent(this,EscritorioActivity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+        btnIniciarSesion.setOnClickListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_inicio_sesion, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Maneja la selección de items con el evento clic
+        return when (item.itemId) {
+            R.id.nav_informacion -> {
+                mostrarInformacion()
+                true
+            }
+            R.id.nav_acerca_de -> {
+                mostrarAcercade()
+                true
+            }
+            R.id.nav_terminos_condiciones -> {
+                mostrarTerminos()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun mostrarTerminos() {
+        startActivity(Intent(this,TerminosActivity::class.java))
+    }
+
+    private fun mostrarAcercade() {
+        startActivity(Intent(this,AcercadeActivity::class.java))
+    }
+
+    private fun mostrarInformacion() {
+        startActivity(Intent(this,InformacionActivity::class.java))
+    }
+}
